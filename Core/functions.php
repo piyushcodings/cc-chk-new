c9934116df107f342424be0a698b5982b451cd74
piyushcodings <118120770+piyushcodings@users.noreply.github.com>
2023-02-22 12:08:24 +0530

    Update functions.php

--- Core/functions.php
+++ Core/functions.php
@@ -24,7 +24,7 @@ function multiexplode($delimiters, $string)
 }
 
 function editMessage ($chatId, $message,$message_id){
-$url = "https://api.telegram.org/bot5980177112:AAF2saZaYIej03yT5_UE3Gbjlpgi1g7tnyw/editMessageText?chat_id=".$chatId."&message_id=".$message_id."&text=".$message."&parse_mode=HTML";
+$url = "https://api.telegram.org/bot5731494604:AAGtUtx5f57kuNkBvqWgaVcDmKORAzn9HD4/editMessageText?chat_id=".$chatId."&message_id=".$message_id."&text=".$message."&parse_mode=HTML";
 file_get_contents($url);      
 }
 
